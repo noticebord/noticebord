5788b87dae48a60fb4ba7b71dc3f1c31a84b226b
sixpeteunder <plenjo15@gmail.com>
2022-04-19 07:37:55 +0300

    Send users more mail

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ use Laravel\{
     Sanctum\HasApiTokens
 };
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens;
     use HasFactory;
